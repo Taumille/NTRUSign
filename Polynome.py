13b84347ae12091ad8e752ea2c4880d9103b8a46
Thomas Bonnefille <thomas.bonnefille@outlook.com>
2023-12-07 20:25:28 +0100

    Naive implementation of polynomial modular inverse
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@outlook.com>

--- Polynome.py
+++ Polynome.py
@@ -1,12 +1,12 @@
 import random
-import time
 import numpy as np
 
 
 class Polynome:
-    def __init__(self, N=503, gen=False, o=0):
+    def __init__(self, N=503, gen=False, o=0, q=2**32):
         self.coeff = np.array([0 for _ in range(0, N)])
         self.N = len(self.coeff)
+        self.q = q
         if gen:
             self.coeff[o] = 1
 
@@ -14,10 +14,11 @@ class Polynome:
         return self.N
 
     def __add__(self, other):
-        res = Polynome(N=max(len(self), len(other)))
+        res = Polynome(N=max(len(self), len(other)), q=self.q)
 
         for k in range(min(len(self), len(other))):
             res.coeff[k] = self.coeff[k] + other.coeff[k]
+            res.coeff[k] = res.coeff[k] % self.q
 
         if min(len(self), len(other)) == len(self):
             for k in range(len(other), len(self)):
@@ -28,18 +29,29 @@ class Polynome:
         return res
 
     def __sub__(self, other):
-        tmp = Polynome(N=other.N)
-        tmp.coeff = -other.coeff
+        tmp = Polynome(N=other.N, q=self.q)
+        tmp.coeff = -other.coeff % q
+        print(f"min :{tmp}")
         return self + tmp
 
-    def conv(self, other):
-        res = Polynome(N=max(len(self), len(other)))
-        for k in range(len(res)):
-            for i in range(len(res)):
-                if i <= k:
-                    res.coeff[k] += self.coeff[i] * other.coeff[k-i]
-                else:
-                    res.coeff[k] += self.coeff[i] * other.coeff[len(self)+k-i]
+    def __mul__(self, other):
+        if isinstance(other, int):
+            res = Polynome(N=self.N, q=self.q)
+            res.coeff = self.coeff * other
+            res.coeff % self.q
+        elif isinstance(other, Polynome):
+            res = Polynome(N=max(len(self), len(other)), q=self.q)
+            for k in range(len(res)):
+                for i in range(len(res)):
+                    try:
+                        if i <= k:
+                            res.coeff[k] += self.coeff[i] * other.coeff[k-i]
+                        else:
+                            res.coeff[k] += self.coeff[i] * other.coeff[len(self)+k-i]
+                        res.coeff[k] = res.coeff[k] % self.q
+                    except IndexError:
+                        res.coeff[k] += 0
+                        print("Warning multiply error")
 
         return res
 
@@ -50,49 +62,32 @@ class Polynome:
         s += str(self.coeff[0])
         return s
 
-    def __mul__(self, other):
-        if isinstance(other, int):
-            res = Polynome(N=self.N)
-            res.coeff = self.coeff * other
+    def ord(self):
+        for i in range(1, self.N+1):
+            if self.coeff[self.N-i] != 0:
+                return self.N-i
+        return -1
 
-        elif isinstance(other, Polynome):
-            res = Polynome(N=max(len(self), len(other)))
-            for k in range(len(res)):
-                for i in range(len(res)):
-                    if i <= k:
-                        res.coeff[k] += self.coeff[i] * other.coeff[k-i]
-        else:
-            raise TypeError(f"Unsupported operand type for Polynome : {type(other)}")
+    def mod(self, q):
+        for k in range(self.ord() + 1):
+            self.coeff[k] = self.coeff[k] % q
 
-        return res
-
-
-def euclidDiv(A_in, B_in, q=2**32):
-    # Compute A//B with A and B two polynomes
-    (A, B) = (A_in, B_in)
-    Q = Polynome(A.N)
-    R = A
-
-    order = A.ord() - B.ord()
-
-    for o in range(order, -1, -1):
-        m = R.coeff[B.ord()+o]/B.coeff[B.ord()]
-        if m < 0:
-            m = np.ceil(m)
-        else:
-            m = np.floor(m)
-        m = int(m) % q
-        print(f"m : {m}")
-        R = R - Polynome(N=A.N, gen=True, o=o) * B * m
-        Q.coeff[o] = m
-
-    return (R, Q)
+    def inv(self, q):
+        Q = Polynome(N=self.N, q=self.q)
+        id = Polynome(N=self.N, gen=True, o=0, q=self.q)
+        while np.linalg.norm((self*Q).coeff-id.coeff):
+            Q.coeff[0] += 1
+            for i in range(self.N):
+                if Q.coeff[i] == q:
+                    Q.coeff[i+1] += 1
+                    Q.coeff[i] = 0
+        return Q
 
 
 def randomGenPoly(N=503, inP=False, modq=2**32-1):
-    p = Polynome(N)
+    p = Polynome(N, q=modq)
     for i in range(N):
-        p.coeff[i] = random.randint(0, N)
+        p.coeff[i] = random.randint(-1, 1)
     if inP:
         # P equiv Q[X^N-1]
         p.coeff[0] += p.coeff[-1]
