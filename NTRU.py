2d7230ea899e202350700e0d17bdaa61656bb1fc
Thomas Bonnefille <thomas.bonnefille@outlook.com>
2023-12-28 10:29:13 +0100

    Basic hash function using SHA-1 Algorithm
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@outlook.com>

---
+++ NTRU.py
@@ -0,0 +1,22 @@
+from KeyGenerator import KeyPair
+from Polynome import Polynome
+import hashlib
+import numpy as np
+
+
+def convertToPolynome(s: bytes):
+    m = hashlib.sha1()
+    m.update(s)
+    m = m.hexdigest()
+    N = 2**(int(np.ceil(np.log2(8*len(m)))))
+    p = Polynome(N=N)
+    m = ''.join(format(ord(x), 'b') for x in m)
+    for i in range(len(m)):
+        p.coeff[i] = int(m[i])
+    return p
+
+
+if __name__ == "__main__":
+    infile = open("Alice.pdf", "rb")
+    data = infile.read()
+    convertToPolynome(data)
